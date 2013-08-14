e895a001ad37b4967510c0b625e5a66054a183f1
sreelakshmy <sreelakshmy50@gmail.com>
2013-08-14 23:00:35 +0530

    Update server.c

--- server.c
+++ server.c
@@ -1,112 +1,163 @@
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <ctype.h>          
+#include <arpa/inet.h>
+#include <netdb.h>
 
-#define closesocket close
-#define PROTOPORT         5193          /* default protocol port number */
-#define QLEN              6             /* size of request queue        */
+#define PORT 20000
+#define BACKLOG 5
+#define LENGTH 512 
 
-int visits =  0;                        /* counts client connections     */
-/*----------------------------------------------------------------------------
-* Program:      server
-*
-* Purpose:        allocate a socket and then repeatedly execute the folllowing:
-*                          (1) wait for the next connection from a client
-*                          (2) send a short message to the client
-*                          (3) close the connection
-*                          (4) go back to step (1)
-*
-* Syntax:          server [ port ]
-*
-*                            port  - protocol port number to use
-*
-* Note:              The port argument is optional. If no port is specified,
-*                        the server uses the default given by PROTOPORT.
-*
-*----------------------------------------------------------------------------
-*/
-main (argc, argv)
-int           argc;
-char         *argv[];
+void error(const char *msg)
 {
-     struct   hostent   *ptrh;     /* pointer to a host table entry */
-     struct   protoent  *ptrp;     /* pointer to a protocol table entry */
-     struct   sockaddr_in sad;     /* structure to hold server's address */
-     struct   sockaddr_in cad;     /* structure to hold client's address */
-     int      sd, sd2;             /* socket descriptors */
-     int      port;                /* protocol port number */
-     int      alen;                /* length of address */
-     char     buf[1000];           /* buffer for string the server sends */
+    perror(msg);
+    exit(1);
+}
 
-//#ifdef  WIN32
-//#define WSADATA wsaData;
-//#define WSAstartup (0x0101, &wsaData);
-//#endif
-     memset((char  *)&sad,0,sizeof(sad)); /* clear sockaddr structure   */
-     sad.sin_family = AF_INET;            /* set family to Internet     */
-     sad.sin_addr.s_addr = INADDR_ANY;    /* set the local IP address */
-
-     /* Check  command-line argument for protocol port and extract      */
-     /* port number if one is specfied.  Otherwise, use the default     */
-     /* port value given by constant PROTOPORT                          */
+int main ()
+{
     
-     if (argc > 1) {                        /* if argument specified     */
-                     port = atoi (argv[1]); /* convert argument to binary*/
-     } else {
-                      port = PROTOPORT;     /* use default port number   */
-     }
-     if (port > 0)                          /* test for illegal value    */
-                      sad.sin_port = htons((u_short)port);
-     else {                                /* print error message and exit */
-                      fprintf (stderr, "bad port number %s/n",argv[1]);
-                      exit (1);
-     }
+    int sockfd; 
+    int nsockfd; 
+    int num;
+    int sin_size; 
+    struct sockaddr_in cli_adr; 									// structure to hold client's address
+    struct sockaddr_in ser_adr; 									// structure to hold server's address
+    char revbuf[LENGTH]; 										// Receiver buffer
 
-     /* Map TCP transport protocol name to protocol number */
-     
-     if ( ((int)(ptrp = getprotobyname("tcp"))) == 0)  {
-                     fprintf(stderr, "cannot map \"tcp\" to protocol number");
-                     exit (1);
-     }
+    						 
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 )						//The Socket file descriptor
+    {
+        fprintf(stderr, "ERROR: Failed to obtain Socket Descriptor. (errno = %d)\n", errno);
+        exit(1);
+    }
+    else 
+        printf("\nServer:Obtaining socket descriptor successfully.\n");
 
-     /* Create a socket */
-     sd = socket (PF_INET, SOCK_STREAM, ptrp->p_proto);
-     if (sd < 0) {
-                       fprintf(stderr, "socket creation failed\n");
-                       exit(1);
-     }
+    
+    cli_adr.sin_family = AF_INET; 									// Protocol Family
+    cli_adr.sin_port = htons(PORT); 									// Host to Network Short 
+    cli_adr.sin_addr.s_addr = INADDR_ANY;								// AutoFill local address
+    bzero(&(cli_adr.sin_zero), 8);									// Flush the rest of struct
 
-     /* Bind a local address to the socket */
-     if (bind(sd, (struct sockaddr *)&sad, sizeof (sad)) < 0) {
-                        fprintf(stderr,"bind failed\n");
-                        exit(1);
-     }
+                                            // Binding to a special Port 
 
-     /* Specify a size of request queue */
-     if (listen(sd, QLEN) < 0) {
-                        fprintf(stderr,"listen failed\n");
-                         exit(1);
-     }
+    if( bind(sockfd, (struct sockaddr*)&cli_adr, sizeof(struct sockaddr)) == -1 )
+    {
+        fprintf(stderr, "ERROR: Failed to bind Port. (errno = %d)\n", errno);
+        exit(1);
+    }
+    else 
+        printf("\nServer: Binded tcp port sucessfully.\n");
 
-     /* Main server loop - accept and handle requests */
-     fprintf( stderr, "Server up and running.\n");
-     while (1) {
-         alen = sizeof(cad);
-         fprintf( stderr, "SERVER: Waiting for contact ...\n");
-         
-         if (  (sd2=accept(sd, (struct sockaddr *)&cad, &alen)) < 0) {
-                              fprintf(stderr, "accept failed\n");
-                              exit (1);
-           }
-           visits++;
-           sprintf(buf,"This server has been contacted %d time%s\n",
-                        visits, visits==1?".":"s.");
-           fprintf( stderr, "SERVER: %s", buf);
-           send(sd2,buf,strlen(buf),0);
-           closesocket (sd2);
-           }
+                                      // Listen remote or server connect/calling 
+
+    if(listen(sockfd,BACKLOG) == -1)
+    {
+        fprintf(stderr, "ERROR: Failed to listen Port. (errno = %d)\n", errno);
+        exit(1);
+    }
+    else
+        printf ("\nServer: Listening to the port successfully.\n");
+
+    int success = 0;
+    while(success == 0)
+    {
+        sin_size = sizeof(struct sockaddr_in);
+
+                          // Waiting for a connection, and obtain a new socket file despriptor for single connection 
+
+        if ((nsockfd = accept(sockfd, (struct sockaddr *)&ser_adr, &sin_size)) == -1) 
+        {
+            fprintf(stderr, "ERROR: Obtaining new Socket Despcritor. (errno = %d)\n", errno);
+            exit(1);
+        }
+        else 
+            printf("\nServer: Server has got connected from %s.\n", inet_ntoa(ser_adr.sin_addr));
+
+  
+	     system("pwd ; ls");		                // Calling script to display contents in the current directory
+
+		
+			        // File "clienttransfer.txt" from Client receiving at the server 
+
+	printf("\nServer: Receiveing file from client...\n\n File Saved as : 'fromclient.txt'");
+        char* fr_name = "/home/sree/Desktop/fromclient.txt";
+        FILE *fr = fopen(fr_name, "a");
+        if(fr == NULL)
+            printf("\nFile %s Cannot be opened file on server.\n", fr_name);
+        else
+        {
+            bzero(revbuf, LENGTH); 
+            int fr_block_sz = 0;
+            while((fr_block_sz = recv(nsockfd, revbuf, LENGTH, 0)) > 0) 
+            {
+                int write_sz = fwrite(revbuf, sizeof(char), fr_block_sz, fr);
+                if(write_sz < fr_block_sz)
+                {
+                    error("\nFile write failed on server.\n");
+                }
+                bzero(revbuf, LENGTH);
+                if (fr_block_sz == 0 || fr_block_sz != 512) 
+                {
+                    break;
+                }
+            }
+            if(fr_block_sz < 0)
+            {
+                if (errno == EAGAIN)
+                {
+                    printf("Timed out.\n");
+                }
+                else
+                {
+                    fprintf(stderr, "recv() failed due to errno = %d\n", errno);
+                    exit(1);
+                }
+            }
+            printf("\nServer: File :%s received from client!\n",fr_name);
+            fclose(fr); 
+        }
+
+        
+        			// File 'servertransfer.txt' from server being send to Client 
+ 
+
+           printf("\n\nServer: File 'servertransfer' to client");  
+	    char* fs_name = "/home/sree/networking/nw/servertransfer.txt";
+            char sdbuf[LENGTH]; 							// Send buffer
+            printf("\n\nServer: Sending %s to the Client", fs_name);
+            FILE *fs = fopen(fs_name, "r");
+            if(fs == NULL)
+            {
+                fprintf(stderr, "ERROR: File %s not found on server. (errno = %d)\n", fs_name, errno);
+                exit(1);
+            }
+
+            bzero(sdbuf, LENGTH); 
+            int fs_block_sz; 
+            while((fs_block_sz = fread(sdbuf, sizeof(char), LENGTH, fs))>0)
+            {
+                if(send(nsockfd, sdbuf, fs_block_sz, 0) < 0)
+                {
+                    fprintf(stderr, "ERROR: Failed to send file %s. (errno = %d)\n", fs_name, errno);
+                    exit(1);
+                }
+                bzero(sdbuf, LENGTH);
+            }
+            printf("\n\nServer: File: %s sent to client!\n",fs_name);
+            success = 1;
+
+            close(nsockfd);								//closeing the connection established from client side
+            printf("Server : Connection with Client closed.\n");
+            while(waitpid(-1, NULL, WNOHANG) > 0);
+        
+    }
 }
